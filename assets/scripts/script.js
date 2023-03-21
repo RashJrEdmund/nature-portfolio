0d9c01dcc6ad0115fe1037097c0e1758cb41b46d
rash <orashusedmund@gmail.com>
2023-03-21 18:14:29 +0100

    added the dark overlay onclick of the sidenav

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,19 @@
 const menuIcon = document.querySelector('.menu-icon')
 const sideMenu = document.querySelector('.side-carrier')
 const closeIcon = document.querySelector('.close-icon')
+const overlay = document.getElementById('nav_overlay')
 
 menuIcon.addEventListener('click', () => {
   sideMenu.classList.toggle('side-active')
+  overlay.classList.add('active_nav_overlay')
 })
 
 closeIcon.addEventListener('click', () => {
   sideMenu.classList.toggle('side-active')
+  overlay.classList.remove('active_nav_overlay')
+})
+
+overlay.addEventListener('click', () => {
+  overlay.classList.remove('active_nav_overlay')
+  sideMenu.classList.remove('side-active')
 })
